5c706b2f96959295edbfc716bec7eba2ed332f9f
hariharan545 <129192624+hariharan545@users.noreply.github.com>
2025-07-19 15:27:38 +0530

    Update expense_summary_agent.py

--- Bill_Managing_Agent/expense_summary_agent.py
+++ Bill_Managing_Agent/expense_summary_agent.py
@@ -1,1 +1,13 @@
 
+
+def summarize_expenses(categorized_data):
+    total = sum(categorized_data.values())
+    max_category = max(categorized_data, key=categorized_data.get)
+
+    summary = {
+        "Total": total,
+        "Highest Category": max_category,
+        "Details": categorized_data
+    }
+
+    return summary
